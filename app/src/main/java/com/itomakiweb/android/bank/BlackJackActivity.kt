7180baf2ed42d93d132b519464f5bcfac6274acb
adachi-swivel <masanof.adachi@gmail.com>
2019-10-17 23:11:46 +0900

    BLACK JACKへの画面遷移

---
+++ app/src/main/java/com/itomakiweb/android/bank/BlackJackActivity.kt
@@ -0,0 +1,12 @@
+package com.itomakiweb.android.bank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BlackJackActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_black_jack)
+    }
+}
