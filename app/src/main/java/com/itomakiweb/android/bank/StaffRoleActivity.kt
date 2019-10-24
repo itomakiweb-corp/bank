995b5675b85223c7f94b12d746256eeabd7e40c0
ikki <ikki.undine@gmail.com>
2019-10-24 21:49:13 +0900

    Staffrole画面追加

---
+++ app/src/main/java/com/itomakiweb/android/bank/StaffRoleActivity.kt
@@ -0,0 +1,12 @@
+package com.itomakiweb.android.bank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StaffRoleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_role)
+    }
+}
