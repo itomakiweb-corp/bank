3eb06bde785b802724052b534362305e7c6a4d35
tanukinoyu <tanuki.yu22@gmail.com>
2019-10-24 22:35:52 +0900

    showStaffRoleResultボタンの追加（途中）

--- app/src/main/java/com/itomakiweb/android/bank/StaffRoleActivity.kt
+++ app/src/main/java/com/itomakiweb/android/bank/StaffRoleActivity.kt
@@ -2,11 +2,23 @@ package com.itomakiweb.android.bank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_staff_role.*
 
 class StaffRoleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_staff_role)
+
+        val staffList: MutableList<String> = mutableListOf()
+
+        showStaffRoleResult.setOnClickListener {
+            if (nameIkki.isChecked){
+                staffList.add("ikki")
+            }
+
+            staffRoleResult.text = staffList.shuffled().toString()
+        }
+
     }
 }
