06d07a3cb5eaf8c55a717fa5b36ef75d3e25805e
Hide <itomakiweb@users.noreply.github.com>
2019-11-07 07:54:13 +0900

    add createIssue

--- app/src/main/java/com/itomakiweb/android/bank/libraries/entities.kt
+++ app/src/main/java/com/itomakiweb/android/bank/libraries/entities.kt
@@ -5,6 +5,79 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+data class GithubGraphqlInput(
+    val query: String = """mutation(${'$'}input: CreateIssueInput!) {
+    createIssue(input: ${'$'}input) {
+        issue {
+            id
+            url
+            title
+        }
+    }
+}
+""",
+
+    val variables: Any
+)
+
+@JsonClass(generateAdapter = true)
+data class GithubCreateIssueInput(
+    val title: String,
+
+    val body: String,
+
+    val assigneeIds: List<String>,
+
+    val labelIds: List<String>,
+
+    val projectIds: List<String> = BuildConfig.GITHUB_TODO_PROJECT_IDS.toList(),
+
+    val milestoneId: String = BuildConfig.GITHUB_MILESTONE_ID,
+
+    val repositoryId: String = BuildConfig.GITHUB_REPOSITORY_ID
+)
+
+@JsonClass(generateAdapter = true)
+data class GithubUpdateIssueInput(
+    val id: String,
+
+    val state: String,
+
+    val title: String,
+
+    val body: String,
+
+    val assigneeIds: List<String>,
+
+    val labelIds: List<String>,
+
+    val projectIds: List<String> = BuildConfig.GITHUB_TODO_PROJECT_IDS.toList(),
+
+    val milestoneId: String = BuildConfig.GITHUB_MILESTONE_ID,
+
+    val repositoryId: String = BuildConfig.GITHUB_REPOSITORY_ID
+)
+
+@JsonClass(generateAdapter = true)
+data class GithubCreateIssueOutput(
+    val createIssue: GithubIssueParent
+)
+
+@JsonClass(generateAdapter = true)
+data class GithubIssueParent(
+    val issue: GithubIssue
+)
+
+@JsonClass(generateAdapter = true)
+data class GithubIssue(
+    val id: String,
+
+    val url: String,
+
+    val title: String
+)
+
+@JsonClass(generateAdapter = true)
 data class GithubUser(
     @Json(name = "node_id")
     val nodeId: String,
@@ -26,6 +99,7 @@ data class GithubRepositoriesOutput(
     @Json(name = "total_count")
     val totalCount: Int
 )
+
 
 @JsonClass(generateAdapter = true)
 data class SlackChatMessageInput(
--- app/src/main/java/com/itomakiweb/android/bank/pages/QuestNewActivity.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/QuestNewActivity.kt
@@ -2,12 +2,73 @@ package com.itomakiweb.android.bank.pages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.itomakiweb.android.bank.BuildConfig
 import com.itomakiweb.android.bank.R
+import com.itomakiweb.android.bank.libraries.GithubApi
+import com.itomakiweb.android.bank.libraries.GithubCreateIssueInput
+import kotlinx.android.synthetic.main.activity_quest_new.*
+import kotlinx.coroutines.runBlocking
 
 class QuestNewActivity : AppCompatActivity() {
+    val priorityMap = mapOf(
+        R.id.priority1 to BuildConfig.GITHUB_PRIORITY1,
+        R.id.priority3 to BuildConfig.GITHUB_PRIORITY3,
+        R.id.priority5 to BuildConfig.GITHUB_PRIORITY5
+    )
+    val costPreMap = mapOf(
+        R.id.costPre0 to BuildConfig.GITHUB_COST_PRE0,
+        R.id.costPre1 to BuildConfig.GITHUB_COST_PRE1,
+        R.id.costPre2 to BuildConfig.GITHUB_COST_PRE2,
+        R.id.costPre3 to BuildConfig.GITHUB_COST_PRE3,
+        R.id.costPre5 to BuildConfig.GITHUB_COST_PRE5,
+        R.id.costPreX to BuildConfig.GITHUB_COST_PREX
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quest_new)
+
+        milestoneId.setText(BuildConfig.GITHUB_MILESTONE_ID)
+        createQuest.setOnClickListener {
+            if (titleQuest.text.isEmpty()) {
+                titleQuest.error = "required"
+                return@setOnClickListener
+            }
+
+            val assigneeIds = if (assignAll.isChecked) {
+                BuildConfig.GITHUB_TODO_ASSIGNEE_IDS.toList()
+            } else {
+                listOf<String>()
+            }
+            val input = GithubCreateIssueInput(
+                title = titleQuest.text.toString(),
+                body = body.text.toString(),
+                assigneeIds = assigneeIds,
+                labelIds = listOf(
+                    priorityMap[priority.checkedRadioButtonId] ?: BuildConfig.GITHUB_PRIORITY3,
+                    costPreMap[costPre.checkedRadioButtonId] ?: BuildConfig.GITHUB_COST_PRE3
+                ),
+                milestoneId = milestoneId.text.toString()
+            )
+            Log.i("test", input.toString())
+            /*
+            */
+        }
+    }
+
+    fun createGithubIssue(input: GithubCreateIssueInput) {
+        // TODO いずれ、処理の共通化を検討
+        runBlocking {
+            try {
+                val message = GithubApi.instance.createIssue(input)
+
+                Log.i("api", message.toString())
+            } catch (e: Exception) {
+                val result = e.message
+
+                Log.w("api", result, e)
+            }
+        }
     }
 }
