0158ea46502c28abef5be1e71581ac794eeda6c5
Hide <itomakiweb@users.noreply.github.com>
2019-11-07 07:29:13 +0900

    add QuestNewActivity base

---
+++ app/src/main/java/com/itomakiweb/android/bank/pages/QuestNewActivity.kt
@@ -0,0 +1,13 @@
+package com.itomakiweb.android.bank.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itomakiweb.android.bank.R
+
+class QuestNewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_new)
+    }
+}
