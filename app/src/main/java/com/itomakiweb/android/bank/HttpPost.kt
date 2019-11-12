20005fd099b6ad5ee237ccc1aaabadc0cfcf780e
tanukinoyu <tanuki.yu22@gmail.com>
2019-11-12 19:59:56 +0900

    http通信のためのクラスファイル追加

--- app/src/main/java/com/itomakiweb/android/bank/HttpPost.kt
+++ app/src/main/java/com/itomakiweb/android/bank/HttpPost.kt
@@ -7,12 +7,24 @@ import okhttp3.RequestBody
 import org.json.JSONObject
 
 class HttpPost {
-    fun post(url: String, postBody: String) {
+    fun post(url: String, body: String): String? {
         val client: OkHttpClient = OkHttpClient.Builder().build()
+
+        val json = JSONObject()
+        json.put("text", body)
+
+        val mediaTypeJson = MediaType.get("application/json; charset=utf-8")
+
+        val postBody = RequestBody.create(mediaTypeJson, json.toString())
 
         val request = Request.Builder()
             .url(url)
-            .post(postBody.)
+            .post(postBody)
+            .build()
+        val response = client.newCall(request).execute()
+
+        val result: String? = response.body()?.string()
+        return result
 
     }
 }
