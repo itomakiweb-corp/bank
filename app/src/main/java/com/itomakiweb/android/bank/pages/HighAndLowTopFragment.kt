03340c692015cecfd6e3e975deaea52533dbcebb
okabekazuyoshi <43435985+bac0907@users.noreply.github.com>
2019-11-14 22:56:21 +0900

    ハイアンドローの画面遷移（③→④）実装　　fix #187
    highAndLowTopFragment でボタン以外を押すと画面遷移する

---
+++ app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowTopFragment.kt
@@ -0,0 +1,25 @@
+package com.itomakiweb.android.bank.pages
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.itomakiweb.android.bank.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HighAndLowTopFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_high_and_low_top, container, false)
+    }
+
+
+}
