7adf9bed575a1560b52b77da022f0b5a0a9c8282
tanukinoyu <tanuki.yu22@gmail.com>
2019-12-05 22:31:38 +0900

    ハイアンドロウのgameフラグメントからの画面遷移

--- app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowGameFragment.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowGameFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.itomakiweb.android.bank.R
+import kotlinx.android.synthetic.main.fragment_high_and_low_game.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +21,18 @@ class HighAndLowGameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_high_and_low_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toHighAndLowResult.setOnClickListener {
+            val fragment = HighAndLowResultFragment()
+            fragmentManager!!.beginTransaction()
+                .replace(R.id.highAndLowFragment, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
 
