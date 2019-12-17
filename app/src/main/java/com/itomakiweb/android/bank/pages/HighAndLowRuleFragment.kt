985f95d90e3e3740adeb215f2612d8fba3b6a724
okabekazuyoshi <43435985+bac0907@users.noreply.github.com>
2019-12-17 22:19:50 +0900

    ハイアンドローのルールを表示させるフラグメントを実装　fix #238
    フラグメントは空っぽのまま

---
+++ app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowRuleFragment.kt
@@ -0,0 +1,26 @@
+package com.itomakiweb.android.bank.pages
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.itomakiweb.android.bank.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HighAndLowRuleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_high_and_low_rule, container, false)
+    }
+
+
+}
