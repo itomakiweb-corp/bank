3593c8f8c69294e25ab6f7ec4c2af9ce0d0f6566
tanukinoyu <tanuki.yu22@gmail.com>
2019-12-22 02:45:14 +0900

    HighAndLowResultFragmentの作成

--- app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowResultFragment.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowResultFragment.kt
@@ -1,12 +1,14 @@
 package com.itomakiweb.android.bank.pages
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.itomakiweb.android.bank.R
+import kotlinx.android.synthetic.main.fragment_high_and_low_result.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +21,29 @@ class HighAndLowResultFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_high_and_low_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //ハイアンドローの実装が済んでいないので仮の値を入れています
+        val betMoney = 1000
+        val nextBetMoney = betMoney + 1000
+
+        messageWindowText.text = getString(R.string.resultMoney, nextBetMoney)
+
+        nextButton.setOnClickListener {
+            val fragment = HighAndLowPlayFragment()
+            fragmentManager!!.beginTransaction()
+                .replace(R.id.highAndLowFragmentFrame, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        backButton.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
