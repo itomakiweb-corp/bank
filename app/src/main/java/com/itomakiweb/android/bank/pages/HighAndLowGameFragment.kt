903e7665182fd6c6e9a6d76d6398fce8926b7118
ikki <ikki.undine@gmail.com>
2019-12-23 22:40:33 +0900

    card_backデザインを変更

--- app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowGameFragment.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/HighAndLowGameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.itomakiweb.android.bank.R
+import kotlinx.android.synthetic.main.fragment_high_and_low_game.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +21,15 @@ class HighAndLowGameFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_high_and_low_game, container, false)
     }
+
+    fun changeCards(){
+        drawCard.setImageResource(R.drawable.card_club_01)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 
 }
