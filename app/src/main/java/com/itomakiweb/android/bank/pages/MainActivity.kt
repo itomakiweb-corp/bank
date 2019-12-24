a642a9e2626232a8bd27c322431b3a418b23728a
Hide <itomakiweb@users.noreply.github.com>
2019-12-24 21:31:22 +0900

    add firebase auth

--- app/src/main/java/com/itomakiweb/android/bank/pages/MainActivity.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/MainActivity.kt
@@ -2,18 +2,33 @@ package com.itomakiweb.android.bank.pages
 
 import android.os.Bundle
 import android.util.Log
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.annotation.VisibleForTesting
 import com.google.firebase.firestore.FirebaseFirestore
 import com.itomakiweb.android.bank.R
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        buttonAnonymousSignIn.setOnClickListener {
+            signInAnonymously()
+        }
 
         // 画面が再利用されていない場合のみ、生成
         if (savedInstanceState == null) {
@@ -79,6 +94,156 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.mainFragment, fragment)
             .addToBackStack(null)
             .commit()
+    }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        // updateUI(currentUser)
+    }
+    // [END on_start_check_user]
+
+    private fun signInAnonymously() {
+        showProgressDialog()
+        // [START signin_anonymously]
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success")
+                    val user = auth.currentUser
+                    // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    // updateUI(null)
+                }
+
+                // [START_EXCLUDE]
+                hideProgressDialog()
+                // [END_EXCLUDE]
+            }
+        // [END signin_anonymously]
+    }
+
+    /*
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
+    }
+
+    private fun linkAccount() {
+        // Make sure form is valid
+        if (!validateLinkForm()) {
+            return
+        }
+
+        // Get email and password from the form
+        val email = fieldEmail.text.toString()
+        val password = fieldPassword.text.toString()
+
+        // Create EmailAuthCredential with email and password
+        val credential = EmailAuthProvider.getCredential(email, password)
+
+        // Link the anonymous user to the email credential
+        showProgressDialog()
+
+        // [START link_credential]
+        auth.currentUser?.linkWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "linkWithCredential:success")
+                    val user = task.result?.user
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "linkWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // [START_EXCLUDE]
+                hideProgressDialog()
+                // [END_EXCLUDE]
+            }
+        // [END link_credential]
+    }
+
+    private fun validateLinkForm(): Boolean {
+        var valid = true
+
+        val email = fieldEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            fieldEmail.error = "Required."
+            valid = false
+        } else {
+            fieldEmail.error = null
+        }
+
+        val password = fieldPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            fieldPassword.error = "Required."
+            valid = false
+        } else {
+            fieldPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        hideProgressDialog()
+        val isSignedIn = user != null
+
+        // Status text
+        if (isSignedIn) {
+            anonymousStatusId.text = getString(R.string.id_fmt, user!!.uid)
+            anonymousStatusEmail.text = getString(R.string.email_fmt, user.email)
+        } else {
+            anonymousStatusId.setText(R.string.signed_out)
+            anonymousStatusEmail.text = null
+        }
+
+        // Button visibility
+        buttonAnonymousSignIn.isEnabled = !isSignedIn
+        // buttonAnonymousSignOut.isEnabled = isSignedIn
+        // buttonLinkAccount.isEnabled = isSignedIn
+    }
+    */
+
+    companion object {
+        private const val TAG = "AnonymousAuth"
+    }
+
+    @VisibleForTesting
+    val progressDialog by lazy {
+        ProgressDialog(this)
+    }
+
+    fun showProgressDialog() {
+        progressDialog.setMessage(getString(R.string.loading))
+        progressDialog.isIndeterminate = true
+        progressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        if (progressDialog.isShowing) {
+            progressDialog.dismiss()
+        }
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        hideProgressDialog()
     }
 
     override fun onBackPressed() {
