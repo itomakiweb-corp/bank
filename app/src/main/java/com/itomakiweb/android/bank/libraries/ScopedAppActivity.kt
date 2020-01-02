ad54374fdb85f29de1bb12355f23bd8f95b0346e
Hide <itomakiweb@users.noreply.github.com>
2020-01-02 22:51:36 +0900

    move progress bar to ScopedAppActivity

--- app/src/main/java/com/itomakiweb/android/bank/libraries/ScopedAppActivity.kt
+++ app/src/main/java/com/itomakiweb/android/bank/libraries/ScopedAppActivity.kt
@@ -1,6 +1,12 @@
 package com.itomakiweb.android.bank.libraries
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
+import com.itomakiweb.android.bank.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -9,6 +15,37 @@ import kotlinx.coroutines.cancel
  * should use when async
  */
 abstract class ScopedAppActivity: AppCompatActivity(), CoroutineScope by MainScope() {
+
+    // for progress bar
+    @VisibleForTesting
+    val progressDialog by lazy {
+        ProgressDialog(this)
+    }
+
+    fun showProgressDialog() {
+        progressDialog.setMessage(getString(R.string.loading))
+        progressDialog.isIndeterminate = true
+        progressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        if (progressDialog.isShowing) {
+            progressDialog.dismiss()
+        }
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        hideProgressDialog()
+    }
+
+
+    // for coroutine
     override fun onDestroy() {
         super.onDestroy()
         cancel() // CoroutineScope.cancel
--- app/src/main/java/com/itomakiweb/android/bank/pages/MainActivity.kt
+++ app/src/main/java/com/itomakiweb/android/bank/pages/MainActivity.kt
@@ -20,8 +20,9 @@ import com.google.firebase.firestore.FieldValue
 import com.itomakiweb.android.bank.BuildConfig
 import com.itomakiweb.android.bank.libraries.Cloud
 import com.itomakiweb.android.bank.libraries.Ref
+import com.itomakiweb.android.bank.libraries.ScopedAppActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ScopedAppActivity() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -274,33 +275,6 @@ class MainActivity : AppCompatActivity() {
         // buttonLinkAccount.isEnabled = isSignedIn
     }
     */
-
-    @VisibleForTesting
-    val progressDialog by lazy {
-        ProgressDialog(this)
-    }
-
-    fun showProgressDialog() {
-        progressDialog.setMessage(getString(R.string.loading))
-        progressDialog.isIndeterminate = true
-        progressDialog.show()
-    }
-
-    fun hideProgressDialog() {
-        if (progressDialog.isShowing) {
-            progressDialog.dismiss()
-        }
-    }
-
-    fun hideKeyboard(view: View) {
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        hideProgressDialog()
-    }
 
     override fun onBackPressed() {
 
